67ab94eef31040cda29fbf173ccc763219858e01
MarkosPapas <markospapas123@gmail.com>
2018-10-06 16:46:55 +0100

    Interface Test

--- public_html/Index.php
+++ public_html/Index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
+<head> 
+    <link href="css/style.css" rel="stylesheet">
+    <title>First Catering ltd</title>
+</head>
 <html>
 <body>
+    <div class="header">
+        <img src="img/logo.png">
+        <h1> Welcome to First Catering ltd <h1>
+        <h5>Sign in</h5>
+    </div>
 
-<?php
-echo phpinfo();
-print("O hi Mark");
-?>
-
+    <div class="flex-container">
+        <div>
+            <button>Companies</button>
+        </div>
+         <div>
+             <button>Employees</button>
+         </div>
+        <div>
+            <button>Company CSV Upload</button>
+        </div>  
+    </div>
 </body>
 </html>
